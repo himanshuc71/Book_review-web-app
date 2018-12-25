8becb6d02e03ea49c0cc7191fead7aea8114cd99
Himanshu <36586647+himanshuc71@users.noreply.github.com>
2018-12-25 15:29:17 -0800

    minor fixations

--- import.py
+++ import.py
@@ -3,6 +3,7 @@ import os
 
 from flask import Flask, render_template, request
 from models import *
+from application import DATABASE_URL
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
@@ -12,13 +13,14 @@ db.init_app(app)
 def main():
     f = open("books.csv")
     reader = csv.reader(f, delimiter=',')
+    next(reader)                                # skips the top line with headings
     for ISBN, title, author, date in reader:
-        book = Book(ISBN_num = ISBN, title = title, author = author, publication_year = date)
+        book = Book(ISBN_num = ISBN, title = title, author = author, publication_year = int(date))
         db.session.add(book)
         print(f"Added book with {ISBN}, title : {title}, author: {author}, publication year: {date}.")
     print("commiting")
     db.session.commit()
-    print("committed")
+    print("committed all data")
 if __name__ == "__main__":
     with app.app_context():
         main()
