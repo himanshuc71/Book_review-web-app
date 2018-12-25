278f4a17825b0db70b19adb20af2f6374d8a2921
Himanshu <36586647+himanshuc71@users.noreply.github.com>
2018-12-25 15:31:09 -0800

    env var removed

--- create.py
+++ create.py
@@ -2,9 +2,10 @@ import os
 
 from flask import Flask, render_template, request
 from models import *
+from application import DATABASE_URL
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
